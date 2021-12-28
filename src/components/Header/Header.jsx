e4b19305ac2fb044bca11f13458c8421554e3e2b
natnaelh14 <natnaelh14@github.com>
2021-12-28 13:47:11 -0500

    created styled number component and searchbox component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import NinthLogo from './ninth-logo.png';
 
 const Header = () => {
     return (
         <div>
-            
+            <img style={{ height: '100px', width: '100px' }} src={NinthLogo} alt='ninth-digit-logo' />
         </div>
     )
 }
